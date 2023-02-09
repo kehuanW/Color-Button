6464f727aea40ef06fe5c4293214fc147076e521
Kehuan <kehuanwang123@gmail.com>
2023-02-09 20:20:29 +1030

    test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Create a virtual DOM for the argument
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // find element by display text; rehular expression; i: insenisive
+  const linkElement = screen.getByText(/learn testing/i); //error
+  // Assertion
+  // expect: Jest global, start the assertion
+  // toBeInTheDocument: matcher Jest-DOM
+  // jest-dom comes with create-react-app, erc/setupTest.js imports it before each test, makes matcher available
+  // DOM-based matchers
   expect(linkElement).toBeInTheDocument();
 });
