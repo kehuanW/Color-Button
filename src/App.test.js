1722da12412ca6c5160d9e8161903e2e21beabb2
Kehuan <kehuanwang123@gmail.com>
2023-02-09 21:57:13 +1030

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  // Create a virtual DOM for the argument
-  render(<App />);
-  // find element by display text; rehular expression; i: insenisive
-  const linkElement = screen.getByText(/learn testing/i); //error
-  // Assertion
-  // expect: Jest global, start the assertion
-  // toBeInTheDocument: matcher Jest-DOM
-  // jest-dom comes with create-react-app, erc/setupTest.js imports it before each test, makes matcher available
-  // DOM-based matchers
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+// Create a virtual DOM for the argument
+// render(<App />);
+// Find element by display text; rehular expression; i: insenisive
+// const linkElement = screen.getByText(/learn react/i); //error
+
+// Assertion
+// expect: Jest global, start the assertion
+// toBeInTheDocument: matcher Jest-DOM; DOM-based matchers
+// jest-dom comes with create-react-app, erc/setupTest.js imports it before each test, makes matcher available
+// expect(linkElement).toBeInTheDocument();
+
+// We seldom use new Error. We prefer to use assertion.
+// throw new Error("throw error test")
+// });
+
+test('button has correct initial color, and update when clicked', () => {
+  const { container } = render(<App />);
+  logRoles(container); // can help you to know the roles in the App
+
+  // expect the button text to be "Change to Red"
+  const colorButton = screen.getByRole('button', { name: "Change to Blue" });
+
+  // expect the bg color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  //click button
+  fireEvent.click(colorButton);
+
+  // expect the bg color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be "Change to Red"
+  expect(colorButton).toHaveTextContent("Change to Red")
+})
