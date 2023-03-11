9bc36e8b58a194fdc7779204809fc5adce0d563b
Kehuan <kehuanwang123@gmail.com>
2023-03-11 16:13:10 +1030

    commit

--- in-decrease/src/components/AwesomeCounter.jsx
+++ in-decrease/src/components/AwesomeCounter.jsx
@@ -1,7 +1,27 @@
-const AwesomeCounter = () => {
-    return <div>
-        Awesome Counter
-    </div>
+import { useState } from 'react';
+
+const AwesomeCounter = ({ initialValue }) => {
+    const [count, setCount] = useState(initialValue ?? 0);
+    const handleAdd = () => {
+        setCount(count => count + 1);
+    }
+    const handleRemove = () => {
+        setCount(count => {
+            if (count > 0) {
+                count -= 1;
+            }
+            return count;
+        });
+    }
+
+    return (
+        <div>
+            <h1>Awesome Counter</h1>
+            <button onClick={handleAdd}>Add</button>
+            <span>{count ?? 0}</span>
+            <button onClick={handleRemove}>Remove</button>
+        </div>
+    )
 }
 
 export default AwesomeCounter;
